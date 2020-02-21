fcce36c92a6512e1cf5ff224aa6f37200d91233c
Vladimir Shirmanov <vladimir.shirmanov@gmail.com>
2020-02-21 22:19:53 +0200

    initial commit

---
+++ AdventureWorks.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventureWorks.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
